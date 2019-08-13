f42a2fd4372f625b1b08e2fea97cab4c58628e81
jps7777777 <841590610@qq.com>
2019-08-13 17:12:24 +0800

    初始化内容

---
+++ application/index/controller/User.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2019-8-13 0013
+ * Time: 17:10
+ */
+
+namespace app\index\controller;
+
+
+class User extends Base
+{
+
+    public function add_user(){
+        return "here ...";
+    }
+
+
+}
