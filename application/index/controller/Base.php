6d560ddbe3392a15b2357d7da2c5b00c5c1a6dc5
jps7777777 <841590610@qq.com>
2019-08-13 17:08:58 +0800

    初始化内容

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -13,7 +13,27 @@ use think\Controller;
 
 class Base extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct(null);
+        header('Access-Control-Allow-Origin:*');
+    }
 
+    /**
+     * 数据统一分装返回json
+     * @param null $info
+     * @param string $message
+     * @param string $status
+     */
+    protected function json($info = null, $message = "SUCCESS", $status = "1") {
+        $data['status'] = $status;
+        $data['message'] = $message;
+        $data['time'] = time();
+        $data['data'] = $info;
+        // 返回JSON数据格式到客户端 包含状态信息
+        header('Content-Type:application/json; charset=utf-8');
+        exit(json_encode($data, JSON_UNESCAPED_UNICODE));
+    }
 
 
 }
